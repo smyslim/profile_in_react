be2b4be3d8ac42fbad4c858ddb94e0e415a1ccaa
smyslim <smyslim@gmail.com>
2020-10-16 17:08:08 +0300

    2020_10_16

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Profile = ()=>{
+    return <div className="row">
+        <div className="col-2">
+            <img src="https://i2.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1" width="100%" alt=""/>
+        </div>
+        <div className="col-10">
+            <h1>Иван Иванов</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet aut cum dicta ea explicabo fugiat fugit hic, iure maiores modi nemo non omnis perferendis perspiciatis porro quam reprehenderit totam! Amet ex id, ipsam minus nemo non odit perspiciatis quisquam. Animi beatae consequatur explicabo hic, illo ipsam reiciendis vel! Quos?</p>
+        </div>
+
+    </div>
+}
